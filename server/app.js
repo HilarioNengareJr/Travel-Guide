7ff65e1280178567fd0ef4f7f0b2af4ca9d67ca4
Hilario Nengare <hnengare@gmail.com>
2024-03-02 06:10:00 +0300

    Intergrated api endpoint to Foursquare

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const path = require('path');
 
 const app = express();
 
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -18,10 +20,18 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    console.log(req.body);
     const inputData = req.body.input;
     console.log(`successfully retrieved user input ${inputData}`)
     res.json({success: true, message: "Successful"})
 })
+
+async function getPlacesData(inputData){
+
+    const clientId = 'ZW1PUYYK335DQCTOTT4WL255GABOXG4RHHI23KYA2XETKUTO';
+    const clientSecret = 'FPSZ2CUAPBQO5G0BXTL23ZNUFV04DBGEETJB2A1TIWTL2CZJ';
+    const apiUrl = '';
+
+
+}
 
 module.exports = app;
